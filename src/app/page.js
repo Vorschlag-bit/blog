cfb57ad7aead99eb8dcda2b2375f77fda33fef8c
Vorschlag-bit <bustout@naver.com>
2025-11-21 15:53:25 +0900

    feat: npm install, next.js 설치, 기본 레이아웃(header, footer, main) 구조 잡기

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    // Tailwind CSS(className="...")
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Vorschlag 개발 블로그 🚀
+      </h1>
+      <p className="mt-4 text-gray-600">
+        Hello-World! Next.js로 바닥부터 만드는 기술 블로그
+      </p>
+    </div>
+  )
+}
