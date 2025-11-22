598e25e41c3cf38ecfbe7ec6ef0211b95ac3d528
Vorschlag-bit <bustout@naver.com>
2025-11-22 11:52:16 +0900

    feat: galmuri font 적용

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko" suppressHydrationWarning>
-      <body className="max-w-3xl mx-auto p-4 dark:bg-gray-900 dark:text-gray-100">
+      <head>
+        {/** 갈무리 폰트 불러오기 */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/galmuri/dist/galmuri.css"
+        />
+      </head>
+      <body className="max-w-3xl mx-auto p-4 dark:bg-gray-900 dark:text-gray-100 font-[Galmuri11]">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {/* 여기에 헤더 추가 */}
           <header className="border-b py-4 mb-8 flex justify-between items-center">
