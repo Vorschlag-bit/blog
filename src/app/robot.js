2938ef269504d64fa69ccab580ce5d8ee7fab789
Vorschlag-bit <bustout@naver.com>
2025-11-27 15:53:22 +0900

    fix: robot.txt 규칙 수정

--- src/app/robot.js
+++ src/app/robot.js
@@ -1,16 +1,36 @@
 import { userAgent } from "next/server";
 
 export default function robots() {
+    const baseUrl = `https://vorschlag-blog.vercel.app`
     return {
-        rules: {
-            // 모든 로봇에게 허용
-            userAgent: '*', 
-            // 모든 페이지 허용
-            allow: '/',
-            // (예시) 특정 페이지는 막고 싶을 때
-            disallow: '/private',
-        },
+        rules: [
+            {
+                // 구글 크롤러 규칙
+                userAgent: 'Googlebot', 
+                // 모든 페이지 허용
+                allow: [
+                    '/',
+                    '/posts/*'
+                ],
+                // (예시) 특정 페이지는 막고 싶을 때
+                disallow: [
+                    '/private/*'
+                ],
+            },
+            {
+                // 다른 크롤러 규칙
+                userAgent: '*',
+                allow: [
+                    '/',
+                    '/posts/*'
+                ],
+                disallow: [
+                    '/private/*'
+                ]
+            }
+        ],
         // sitemap을 만들어줘야 로봇이 길을 안 헤맨다
-        sitemap: `https://vorschlag-blog.vercel.app/sitemap.xml`
+        sitemap: `${baseUrl}/sitemap.xml`,
+        host: baseUrl
     }
 }
