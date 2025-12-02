936e8f362c1d10eab43420e28cfbb8c40affe5b4
Vorschlag-bit <bustout@naver.com>
2025-12-02 23:38:54 +0900

    fix: useSearchParams를 Suspense 태그로 감싸도록 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import GlobalLoader from "@/components/GlobalLoader";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ThemeToggle from "@/components/ThemeToggle";
 import Link from "next/link";
+import { Suspense } from "react";
 import "./globals.css";
 import "highlight.js/styles/github-dark.css";
 import 'katex/dist/katex.min.css';
@@ -66,45 +67,48 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="dark:bg-gray-900 dark:text-gray-100 font-[Galmuri11]">
-        <LoadingProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {/** 전역 로딩 컴포넌트 배치 */}
-            <GlobalLoader />
-            {/* 여기에 헤더 추가 */}
-            <header className="max-w-5xl mx-auto px-4 border-b py-4 flex justify-between items-center">
-              {/** 마스코트 추가 */}
-              <Link href="/" className="flex items-center gap-3 group">
-                <div className="relative w-8 h-8 sm:w-10 sm:h-10 transition-transform group-hover:-rotate-12">
-                  <Image
-                    src="/images/icon.png"
-                    alt="Mascot"
-                    fill
-                    className="object-contain" /** 비율 유지 */
-                    style={{ imageRendering: "pixellated" }}
-                  />
-                </div>
-                <h1 className="hover-glitch text-xl font-bold cursor-pointer text-black dark:text-white">
-                  DevLog
-                </h1>
-              </Link>
-              <nav className="flex items-center gap-4">
-                <Link href="/about">About</Link>
-                <Link href="https://github.com/Vorschlag-bit">
-                <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path fill="currentColor" d="M5 2h4v2H7v2H5V2Zm0 10H3V6h2v6Zm2 2H5v-2h2v2Zm2 2v-2H7v2H3v-2H1v2h2v2h4v4h2v-4h2v-2H9Zm0 0v2H7v-2h2Zm6-12v2H9V4h6Zm4 2h-2V4h-2V2h4v4Zm0 6V6h2v6h-2Zm-2 2v-2h2v2h-2Zm-2 2v-2h2v2h-2Zm0 2h-2v-2h2v2Zm0 0h2v4h-2v-4Z"/> </svg>
+        {/** useSearchParams는 <Suspense> 태그로 감싸야 함. URL 파라미터를 읽는 동안 보여줄 아주 짧은 화면이 필요하나, 전역 로딩이 존재하므로 화면 없어도 됌. */}
+        <Suspense fallback={null}>
+          <LoadingProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              {/** 전역 로딩 컴포넌트 배치 */}
+              <GlobalLoader />
+              {/* 여기에 헤더 추가 */}
+              <header className="max-w-5xl mx-auto px-4 border-b py-4 flex justify-between items-center">
+                {/** 마스코트 추가 */}
+                <Link href="/" className="flex items-center gap-3 group">
+                  <div className="relative w-8 h-8 sm:w-10 sm:h-10 transition-transform group-hover:-rotate-12">
+                    <Image
+                      src="/images/icon.png"
+                      alt="Mascot"
+                      fill
+                      className="object-contain" /** 비율 유지 */
+                      style={{ imageRendering: "pixellated" }}
+                    />
+                  </div>
+                  <h1 className="hover-glitch text-xl font-bold cursor-pointer text-black dark:text-white">
+                    DevLog
+                  </h1>
                 </Link>
-                <ThemeToggle/>
-              </nav>
-            </header>
-            {/** 일반 페이지면 main/layout.js, 404면 not-fount.js */}
-            <div className="flex-1 w-full">
-              {children}
-            </div>
-            {/** footer 추가 */}
-            <footer className="max-w-5xl mx-auto px-4 border-t py-4 mt-10 text-center text-gray-500 text-sm">
-              © 2025 Vorschlag Tech Blog
-            </footer>
-          </ThemeProvider>
-        </LoadingProvider>
+                <nav className="flex items-center gap-4">
+                  <Link href="/about">About</Link>
+                  <Link href="https://github.com/Vorschlag-bit">
+                  <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path fill="currentColor" d="M5 2h4v2H7v2H5V2Zm0 10H3V6h2v6Zm2 2H5v-2h2v2Zm2 2v-2H7v2H3v-2H1v2h2v2h4v4h2v-4h2v-2H9Zm0 0v2H7v-2h2Zm6-12v2H9V4h6Zm4 2h-2V4h-2V2h4v4Zm0 6V6h2v6h-2Zm-2 2v-2h2v2h-2Zm-2 2v-2h2v2h-2Zm0 2h-2v-2h2v2Zm0 0h2v4h-2v-4Z"/> </svg>
+                  </Link>
+                  <ThemeToggle/>
+                </nav>
+              </header>
+              {/** 일반 페이지면 main/layout.js, 404면 not-fount.js */}
+              <div className="flex-1 w-full">
+                {children}
+              </div>
+              {/** footer 추가 */}
+              <footer className="max-w-5xl mx-auto px-4 border-t py-4 mt-10 text-center text-gray-500 text-sm">
+                © 2025 Vorschlag Tech Blog
+              </footer>
+            </ThemeProvider>
+          </LoadingProvider>
+        </Suspense>
       </body>
     </html>
   );
