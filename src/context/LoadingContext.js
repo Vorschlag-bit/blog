cee4b96b6c708ba30646b0b899c90274fc50cc0a
Vorschlag-bit <bustout@naver.com>
2025-12-07 22:32:59 +0900

    fix: path 변경 추적하지 않고, 상세 페이지 img 태그 상태 추적으로 수정

--- src/context/LoadingContext.js
+++ src/context/LoadingContext.js
@@ -1,19 +1,19 @@
 "use client"
 
 import { createContext, useContext, useState, useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
+// import { usePathname, useSearchParams } from "next/navigation"
 
 const LoadingContext = createContext()
 
 export function LoadingProvider({ children }) {
     const [isLoading, setIsLoading] = useState(false)
-    const pathName = usePathname()
-    const searchParams = useSearchParams()
+    // const pathName = usePathname()
+    // const searchParams = useSearchParams()
 
-    // 페이지 경로가 바뀌면(이동 완료 시) 로딩 끄기
-    useEffect(() => {
-        setIsLoading(false)
-    }, [pathName, searchParams])
+    // 더이상 path 변경으로 로딩을 감지하지 않음, 대신 상세 페이지의 모든 img 태그 다운로드를 체크하도록 수정
+    // useEffect(() => {
+    //     setIsLoading(false)
+    // }, [pathName, searchParams])
 
     return (
         <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
