0d365cad09b597f5549c3e93522d385216c45b87
Vorschlag-bit <bustout@naver.com>
2025-12-21 01:34:10 +0900

    fix: robots.js 모든 거 허용하도록 수정

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,32 +1,11 @@
 export default function robots() {
-    const baseUrl = `https://vorschlag-blog.vercel.app`
-    return {
-        rules: [
-            {
-                // 구글 크롤러 규칙
-                userAgent: 'Googlebot', 
-                // 모든 페이지 허용
-                allow: [
-                    '/',
-                ],
-                // (예시) 특정 페이지는 막고 싶을 때
-                disallow: [
-                    '/private/'
-                ],
-            },
-            {
-                // 다른 크롤러 규칙
-                userAgent: '*',
-                allow: [
-                    '/posts/'
-                ],
-                disallow: [
-                    '/private/'
-                ]
-            }
-        ],
-        // sitemap을 만들어줘야 로봇이 길을 안 헤맨다
-        sitemap: `${baseUrl}/sitemap.xml`,
-        host: baseUrl
-    }
+const baseUrl = 'https://vorschlag-blog.vercel.app'
+
+return {
+    rules: {
+    userAgent: '*',
+    allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+}
 }
