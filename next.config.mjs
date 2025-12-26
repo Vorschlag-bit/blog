283a5735d95d4017ff553619a352f2014da44907
DESKTOP-DALU0U2\uuhan <uuhandie@gmail.com>
2025-12-27 04:05:00 +0900

    fix: .\next.config.mjs 다시 turbopack으로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // node_modules에서 오는 경고 무시하기
-    config.ignoreWarnings = [
-      { module: /node_modules/ },
-      { file: /node_modules/ },
-    ];
-    return config;
-  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
