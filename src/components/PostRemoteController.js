eb22a61a0f7f70c8ea19046e88f5adce5d751560
vorschlag-bit <bustout@naver.com>
2026-01-02 18:20:24 +0900

    fix: PostRemoteController 닫힘 버튼 추가 밑 CSS 수정

--- src/components/PostRemoteController.js
+++ src/components/PostRemoteController.js
@@ -30,7 +30,7 @@ export default function PostRemoteControl() {
         if (!contentArea) return;
 
         // h1,h2,h3 태그 수집
-        const elements = contentArea.querySelectorAll("h2, h3, h4")
+        const elements = contentArea.querySelectorAll("h2, h3, h4 h5")
         
         // console.log(`찾은 요소들 개수: ${elements.length}`)
         // console.log(`찾은 요소: ${Array.from(elements)}`)
@@ -119,87 +119,105 @@ export default function PostRemoteControl() {
     // 목차가 없다면 렌더링 x
     if (headings.length === 0) return null
 
-    const ToggleButton = () => (
-    <button 
-        onClick={handleOnClick}
-        // 열린 상태(닫힌 버튼) : 닫힌 상태(열림 버튼)
-        className={!isClosed ? "border border-red-500 dark:border-red-400 hover:opacity-70 transition-opacity" : "border-0 hover:opacity transition-opacity"}
-        aria-label={!isClosed ? "목차 닫기" : "목차 열기"}
-    > 
-        {!isClosed ? (
-            // 열린 상태
-            <svg className="w-6 text-red-400 dark:text-red-300" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M5 5h2v2H5V5zm4 4H7V7h2v2zm2 2H9V9h2v2zm2 0h-2v2H9v2H7v2H5v2h2v-2h2v-2h2v-2h2v2h2v2h2v2h2v-2h-2v-2h-2v-2h-2v-2zm2-2v2h-2V9h2zm2-2v2h-2V7h2zm0 0V5h2v2h-2z" fill="currentColor"/> </svg>
-        ): (
-            // 닫힌 상태
-            <svg className="w-6 border text-green-400 dark:text-green-300" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M5 3h6v2H5v14h14v-6h2v8H3V3h2zm8 0h8v8h-2V7h-2V5h-4V3zm0 8h-2v2H9v2h2v-2h2v-2zm4-4h-2v2h-2v2h2V9h2V7z" fill="currentColor"/> </svg>
-        )}
-    </button>
-    )
+    // const ToggleButton = () => (
+    // <button 
+    //     onClick={handleOnClick}
+    //     // 열린 상태(닫힌 버튼) : 닫힌 상태(열림 버튼)
+    //     className={!isClosed ? "border border-red-500 dark:border-red-400 hover:opacity-70 transition-opacity" : "border-0 hover:opacity transition-opacity"}
+    //     aria-label={!isClosed ? "목차 닫기" : "목차 열기"}
+    // > 
+    //     {!isClosed ? (
+    //         // 열린 상태
+    //         <svg className="w-6 text-red-400 dark:text-red-300" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M5 5h2v2H5V5zm4 4H7V7h2v2zm2 2H9V9h2v2zm2 0h-2v2H9v2H7v2H5v2h2v-2h2v-2h2v-2h2v2h2v2h2v2h2v-2h-2v-2h-2v-2h-2v-2zm2-2v2h-2V9h2zm2-2v2h-2V7h2zm0 0V5h2v2h-2z" fill="currentColor"/> </svg>
+    //     ): (
+    //         // 닫힌 상태
+    //         <svg className="w-6 border text-green-400 dark:text-green-300" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M5 3h6v2H5v14h14v-6h2v8H3V3h2zm8 0h8v8h-2V7h-2V5h-4V3zm0 8h-2v2H9v2h2v-2h2v-2zm4-4h-2v2h-2v2h2V9h2V7z" fill="currentColor"/> </svg>
+    //     )}
+    // </button>
+    // )
 
     return (
         // 레이아웃: xl 화면(1280px) 이상에서만 보임
         // 메인 콘텐츠 옆에 붙이기 위해 left 계산식 사용
-        <aside className="hidden xl:block absolute top-55 left-full ml-5 h-full font-[Galmuri11] z-40">
+        <aside className="hidden xl:block absolute top-160 left-full ml-3 h-full font-[Galmuri11] z-40">
             <div className="sticky top-10 w-60">
                 {/** 1. 목차 박스 (레트로 스타일) */}
-                <div className="border-2 dark:border-gray-500 bg-white dark:bg-gray-900
-                shadow-[4px_4px_0px_0px_rgba(0,0,0,0.3)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,0.5)] mb-4 p-4 max-h-[70vh] overflow-y-auto">
-                    <div className="flex items-center justify-between mb-4 border-b-2 border-dashed border-gray-300 pb-3"
-                        aria-level="목차"
-                    >
-                        <div className="flex items-center font-bold gap-1">
-                            <svg className="w-6" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 2h12v20H4V2h4zm4 8h-2v2H8V4H6v16h12V4h-4v8h-2v-2z" fill="currentColor"/> </svg>
-                            <span>목차</span>
-                        </div>
-                        <ToggleButton />
-                    </div>
-                    {!isClosed && (
-                        // 닫히지 않은 경우 (기본)
-                        <div>
-                            <ul className="space-y-2">
-                                {headings.map((heading) => (
-                                    <li key={heading.id}
-                                        className={`transition-color ${indentStyles[heading.level]}`}
-                                    >
-                                        <a href={`#${heading.id}`}
-                                            onClick={(e) => handleLinkClick(e,heading.id)}
-                                            className={`flex items-center block truncate cursor-pointer hover:underline ${
-                                                activeId === heading.id
-                                                ? "text-blue-600 font-bold dark:text-blue-400"
-                                                : "text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200"
-                                            }`}
-                                            title={`${heading.text}`}
-                                            aria-level={`${heading.text}`}
+                {!isClosed ? (
+                    <>
+                        <div className="border-2 dark:border-gray-500 bg-white dark:bg-gray-900
+                        shadow-[4px_4px_0px_0px_rgba(0,0,0,0.3)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,0.5)] mb-4 p-3 max-h-[70vh] overflow-y-auto">
+                            <div className="flex items-center justify-between mb-4 border-b-2 border-dashed border-gray-300 pb-3"
+                                aria-level="목차"
+                            >
+                                <div className="flex items-center font-bold gap-1">
+                                    <svg className="w-6" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 2h12v20H4V2h4zm4 8h-2v2H8V4H6v16h12V4h-4v8h-2v-2z" fill="currentColor"/> </svg>
+                                    <span>목차</span>
+                                </div>
+                                <button onClick={handleOnClick} className="border border-red-500 dark:border-red-400">
+                                    <svg className="w-5 text-red-500 dark:text-red-400" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M5 5h2v2H5V5zm4 4H7V7h2v2zm2 2H9V9h2v2zm2 0h-2v2H9v2H7v2H5v2h2v-2h2v-2h2v-2h2v2h2v2h2v2h2v-2h-2v-2h-2v-2h-2v-2zm2-2v2h-2V9h2zm2-2v2h-2V7h2zm0 0V5h2v2h-2z" fill="currentColor"/> </svg>
+                                </button>
+                            </div>
+                            {/* 닫히지 않은 경우 (기본) */}
+                            <div>
+                                <ul className="space-y-2">
+                                    {headings.map((heading) => (
+                                        <li key={heading.id}
+                                            className={`transition-color ${indentStyles[heading.level]}`}
                                         >
-                                            {activeId === heading.id && <span className="mr-1">
-                                                <svg className="w-4 h-4" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 5v2h2V5H8zm4 4V7h-2v2h2zm2 2V9h-2v2h2zm0 2h2v-2h-2v2zm-2 2v-2h2v2h-2zm0 0h-2v2h2v-2zm-4 4v-2h2v2H8z" fill="currentColor"/> </svg>
-                                                </span>}
-                                            {heading.text}
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                            <ScrollProgress />
+                                            <a href={`#${heading.id}`}
+                                                onClick={(e) => handleLinkClick(e,heading.id)}
+                                                className={`flex items-center block truncate cursor-pointer hover:underline ${
+                                                    activeId === heading.id
+                                                    ? "text-blue-600 font-bold dark:text-blue-400"
+                                                    : "text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200"
+                                                }`}
+                                                title={`${heading.text}`}
+                                                aria-level={`${heading.text}`}
+                                            >
+                                                {activeId === heading.id && <span className="mr-1">
+                                                    <svg className="w-4 h-4" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 5v2h2V5H8zm4 4V7h-2v2h2zm2 2V9h-2v2h2zm0 2h2v-2h-2v2zm-2 2v-2h2v2h-2zm0 0h-2v2h2v-2zm-4 4v-2h2v2H8z" fill="currentColor"/> </svg>
+                                                    </span>}
+                                                {heading.text}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <ScrollProgress />
+                            </div>
                         </div>
-                    )}
-                </div>
-                {/** 2. 리모컨 버튼 (맨위/맨아래) */}
-                {!isClosed &&
-                <div className="flex justify-center gap-3">
-                    <button
-                        onClick={scrollToTop}
-                        className="flex items-center border-gray-200 bg-white dark:bg-gray-800 dark:border-gray-500 p-2 hover:bg-gray-200 dark:hover:bg-gray-500 transition shadow-pixel" title="맨 위로"
-                    aria-label="맨 위로">
-                        <svg className="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M4 6h16V4H4v2zm7 14h2v-8h2v2h2v-2h-2v-2h-2V8h-2v2H9v2H7v2h2v-2h2v8z" fill="currentColor"/> </svg>
-                    </button>
-                    <button
-                        onClick={scrollToBottom}
-                        className="flex items-center border-gray-200 bg-white dark:bg-gray-800 dark:border-gray-500 p-2 hover:bg-gray-200 dark:hover:bg-gray-500 transition shadow-pixel" title="맨 아래로"
-                    aria-label="맨 아래로">
-                        <svg className="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M11 4h2v8h2v2h-2v2h-2v-2H9v-2h2V4zm-2 8H7v-2h2v2zm6 0v-2h2v2h-2zM4 18h16v2H4v-2z" fill="currentColor"/> </svg>
-                    </button>
-                </div>
-                }
+                        {/** 2. 리모컨 버튼 (맨위/맨아래) */}
+                        <div className="flex justify-center gap-3">
+                            <button
+                                onClick={scrollToTop}
+                                className="flex items-center border-gray-200 bg-white dark:bg-gray-800 dark:border-gray-500 p-2 hover:bg-gray-200 dark:hover:bg-gray-500 transition shadow-pixel" title="맨 위로"
+                            aria-label="맨 위로">
+                                <svg className="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M4 6h16V4H4v2zm7 14h2v-8h2v2h2v-2h-2v-2h-2V8h-2v2H9v2H7v2h2v-2h2v8z" fill="currentColor"/> </svg>
+                            </button>
+                            <button
+                                onClick={scrollToBottom}
+                                className="flex items-center border-gray-200 bg-white dark:bg-gray-800 dark:border-gray-500 p-2 hover:bg-gray-200 dark:hover:bg-gray-500 transition shadow-pixel" title="맨 아래로"
+                            aria-label="맨 아래로">
+                                <svg className="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M11 4h2v8h2v2h-2v2h-2v-2H9v-2h2V4zm-2 8H7v-2h2v2zm6 0v-2h2v2h-2zM4 18h16v2H4v-2z" fill="currentColor"/> </svg>
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <div 
+                    className="border-2 dark:border-gray-500 bg-white dark:bg-gray-900
+                        shadow-[4px_4px_0px_0px_rgba(0,0,0,0.3)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,0.5)] mb-4 p-3 max-h-[70vh] overflow-y-auto"
+                    >
+                        <div className="flex items-center justify-between"
+                            aria-level="목차">
+                            <div className="flex items-center font-bold gap-1">
+                                <svg className="w-6" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 2h12v20H4V2h4zm4 8h-2v2H8V4H6v16h12V4h-4v8h-2v-2z" fill="currentColor"/> </svg>
+                                <span>목차</span>
+                            </div>
+                            <button onClick={handleOnClick} className="border border-green-500 dark:border-red-400">
+                                <svg className="w-5 text-green-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M11 4h2v7h7v2h-7v7h-2v-7H4v-2h7V4z" fill="currentColor"/> </svg>
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </aside>
     )
