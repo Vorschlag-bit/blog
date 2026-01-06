21f20f894ec63739fac8bbd36ed3b7eca6445eaf
Vorschlag-bit <bustout@naver.com>
2026-01-06 23:57:12 +0900

    feat: 날씨 요청 server actions 함수 구현 임시 완료

--- src/lib/weather.js
+++ src/lib/weather.js
@@ -1,5 +1,4 @@
 "use server"
-
 import { dfs_xy_conv } from "@/app/utils/positionConverter"
 
 /**
@@ -10,7 +9,14 @@ import { dfs_xy_conv } from "@/app/utils/positionConverter"
  * @param { number } y,
  * @param { string } type,
  */
-export default function getWeather({ x, y, type="xy" }) {
+export default async function getWeather({ x, y, type="xy" }) {
+    // 서비스 키
+    const SERVICE_KEY = process.env.WEATHER_API_KEY?.trim()
+    if (!SERVICE_KEY) {
+        console.error(`WEATHER_API_KEY 부재`);
+        return null
+    }
+
     let nx = x
     let ny = y
     if (type !== "xy") {
@@ -22,7 +28,98 @@ export default function getWeather({ x, y, type="xy" }) {
     // 날짜 계산
     const { baseDate_Fcst,baseTime_Fcst,baseDate_Live,baseTime_Live,baseDate_Srt,baseTime_Srt } = get_currentTime()
     // x,y를 기반으로 날씨 조회
-    
+    const makeUrl = (baseUrl, baseDate, baseTime, rows) => {
+        const url = new URL(baseUrl);
+
+        // searchParams.append로 조합하기
+        url.searchParams.append("serviceKey", SERVICE_KEY)
+        url.searchParams.append("pageNo", "1")
+        url.searchParams.append("numOfRows", rows.toString())
+        url.searchParams.append("dataType", "JSON")
+        url.searchParams.append("base_date", baseDate)
+        url.searchParams.append("base_time", baseTime)
+        url.searchParams.append("nx", nx)
+        url.searchParams.append("ny", ny)
+
+        return url.toString();
+    }
+
+    // 1. 초단기 실황 URL
+    const url_live = makeUrl(
+        "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst",
+        baseDate_Live,
+        baseTime_Live,
+        12
+    )
+    // console.log(`초단기 실황 URL: ${url_live}`)
+
+    // 2. 초단기 예보 URL
+    const url_fcst = makeUrl(
+        "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst",
+        baseDate_Fcst,
+        baseTime_Fcst,
+        100
+    )
+    // console.log(`초단기 예보 URL: ${url_fcst}`)
+
+    // 3. 단기 예보 URL
+    const url_srt = makeUrl(
+        "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst",
+        baseDate_Srt,
+        baseTime_Srt,
+        200
+    )
+    // console.log(`단기 예보 URL: ${url_srt}`);
+
+    try {
+        const [resLive, resFcst, resSrt] = Promise.all([
+            fetch(url_live, { next: { revalidate: 600 } }),
+            fetch(url_fcst, { next: { revalidate: 600 } }),
+            fetch(url_fcst, { next: { revalidate: 900 } })
+        ])
+
+        // res 상태 체크 및 안전한 Json 파싱 함수(text -> json)
+        const errorCheck = async (res, name) => {
+            if (!res.ok) {
+                const errorText = await res.text();
+                console.error(`${name} API Error (${res.status}):`, errorText);
+                throw new Error(`${name} API 요청 실패: ${res.status}`);
+            }
+            const text = await res.text();
+            try {
+                return JSON.parse(text);
+            } catch (error) {
+                console.error("API 응답이 JSON 형식이 아님: ", text.subString(0,100));
+                throw new Error('잘못 형식의 응답 도착(Not Json)')
+            }
+        }
+
+        const liveData = await errorCheck(resLive, "초단기실황")
+        const fcstData = await errorCheck(resFcst, "초단기예보")
+        const srtData = await errorCheck(resSrt, "단기예보")
+
+        // 셋 중 하나라도 실패하면 오류
+        if (liveData.response?.header?.resultCode !== '00' || fcstData.response?.header?.resultCode !== '00' || srtData.response?.header?.resultCode !== '00') {
+            console.error("API Error - header.resultCodes(초단기실황,초단기예보,단기예보 순):", 
+                liveData.response?.header?.resultCode,
+                fcstData.response?.header?.resultCode,
+                srtData.response?.header?.resultCode
+            );
+
+            return null
+        }
+
+        const parsedData = parseWeatherData(
+            liveData.response.body.items.item,
+            fcstData.response.body.items.item,
+            srtData.response.body.items.item,
+        )
+
+        return parsedData
+    } catch (err) {
+        console.debug(`weather api debug: `, err);
+        return null
+    }
 }
 
 function get_currentTime() {
@@ -76,4 +173,57 @@ function get_currentTime() {
                 baseDate_Srt: srtParams.date,
                 baseTime_Srt: srtParams.time
             }
+}
+
+// data를 기반으로 날씨를 판별하는 함수
+function parseWeatherData(liveItems, fcstItems, srtItems) {
+    const now = new Date()
+    const kstAbs = now.getTime() + (9 * 60 * 60 * 1000)
+    
+    const baseDate = new Date(kstAbs).toISOString().slice(0,10).replace(/-/g, "")
+
+    // 1. 실황 데이터
+    const liveMap = {}
+    liveItems.forEach(item => {
+        liveMap[item.category] = Number(item.obsrValue);
+    });
+
+    // 2. 예보 데이터(SKY,LGT 추출)
+    const fcstMap = {}
+    // 예보 데이터에서 가장 빠른 시간대만 추출
+    fcstItems.forEach((item) => {
+        // 이미 있다면 pass
+        if (!fcstMap[item.category]) fcstMap[item.category] = Number(item.fcstValue);
+    })
+
+    // 3. 단기 예보 데이터(TMX(최고), TMN(최저))
+    let tmxValue = 0
+    let tmnValue = 0
+    // console.log("baseDate: ", baseDate);
+    
+    srtItems.forEach((item) => {
+        // 오늘 날짜만 확인
+        if (item.fcstDate === baseDate) {
+            if (item.category === 'TMX') {
+                tmxValue = Number(item.fcstValue);
+                // console.log("tmx: ", item.fcstValue);
+            }
+            if (item.category === 'TMN') {
+                tmnValue = Number(item.fcstValue);
+                // console.log("tmn: ", item.fcstValue);
+                
+            }
+        }
+    })
+    
+    return {
+        temperature: liveMap['T1H'].toFixed(1), // 실황 기온
+        tmx: tmxValue.toFixed(1),               // 최고 기온
+        tmn: tmnValue.toFixed(1),               // 최저 기온
+        humidity: liveMap['REH'],    // 실황 습도
+        wind: liveMap['WSD'],        // 실황 풍속
+        PTY: liveMap['PTY'],         // 실황 강수상태 (0: 없음, 1: 비, 2: 눈/비, 3:눈, 5: 빗방울, 6: 빗방울 날림, 7: 눈날림)
+        SKY: fcstMap['SKY'],         // 예보 하늘 상태
+        LGT: fcstMap['LGT'] > 0      // 예보 낙뢰 여부
+    };
 }
